a44caa6bc198d2d9fef28e540e5ca36e5fccb95d
Monine Chan <mochan@students.42wolfsburg.de>
2022-09-06 16:34:57 +0200

    initialization of forks completed for now with array of fork.

--- philo/src/init.c
+++ philo/src/init.c
@@ -6,32 +6,25 @@
 /*   By: mochan <mochan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/05 11:46:12 by mochan            #+#    #+#             */
-/*   Updated: 2022/09/06 16:23:13 by mochan           ###   ########.fr       */
+/*   Updated: 2022/09/06 16:34:15 by mochan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philosophers.h"
 
-static void init_forks(t_prgm *vars, t_philo *philo)
+static void init_forks(t_prgm *vars, t_philo *philo, int n, t_fork *fork)
 {	
 	philo->left_fork = malloc(sizeof(t_fork));
 	philo->right_fork = malloc(sizeof(t_fork));
 
-	philo->right_fork->fork_id = philo->philo_id;
+	fork[n].fork_id = philo->philo_id;
+	fork[n].fork_status = 0;
+	philo->right_fork->fork_id = fork[n].fork_id;
 	if (philo->philo_id != vars->nb_of_philos)
-		philo->left_fork->fork_id = philo->philo_id + 1;
+		philo->left_fork->fork_id = fork[n].fork_id + 1;
 	else if (philo->philo_id == vars->nb_of_philos)
 		philo->left_fork->fork_id = 1;
 }
-
-// static void init_forks(t_prgm *vars, t_philo *philo)
-// {
-// 	philo->right_fork_id = philo->philo_id;
-// 	if (philo->philo_id != vars->nb_of_philos)
-// 		philo->left_fork_id = philo->philo_id + 1;
-// 	else if (philo->philo_id == vars->nb_of_philos)
-// 		philo->left_fork_id = 1;
-// }
 
 static t_philo	**init_philosophers(t_prgm *vars)
 {
@@ -50,14 +43,14 @@ static t_philo	**init_philosophers(t_prgm *vars)
 	{
 		tab_philos[i] = malloc(sizeof(t_philo) * 1);
 		tab_philos[i]->philo_id = i;
-		init_forks(vars, tab_philos[i]);
+		init_forks(vars, tab_philos[i], i, &arr_forks[i]);
 		printf("philosopher %d : RIGHT fork number is %d.\n",
 			tab_philos[i]->philo_id, tab_philos[i]->right_fork->fork_id);
 		printf("philosopher %d : LEFT fork number is %d.\n",
 			tab_philos[i]->philo_id, tab_philos[i]->left_fork->fork_id);
 		i++;
 	}
-	// printf("==========================================================\n");
+	printf("==========================================================\n");
 	return (tab_philos);
 }
 
