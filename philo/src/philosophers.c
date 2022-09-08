f4c14bc94da3dbd75fe2b8d9c2192b9c187b65f1
Monine Chan <mochan@students.42wolfsburg.de>
2022-09-08 13:10:52 +0200

    adding infinite while loop in routine

--- philo/src/philosophers.c
+++ philo/src/philosophers.c
@@ -6,7 +6,7 @@
 /*   By: mochan <mochan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/31 14:44:44 by mochan            #+#    #+#             */
-/*   Updated: 2022/09/07 17:54:41 by mochan           ###   ########.fr       */
+/*   Updated: 2022/09/08 13:09:51 by mochan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,18 @@ void	*routine(void *arg)
 	philo = (t_philo *) arg;
 	if (philo->philo_id % 2)
 		usleep(1000);
-	pthread_mutex_lock(philo->right_fork);
-	take_a_fork(philo);
-	pthread_mutex_lock(philo->left_fork);
-	take_a_fork(philo);
-	eating(philo);
-	pthread_mutex_unlock(philo->right_fork);
-	pthread_mutex_unlock(philo->left_fork);
-	sleeping(philo);
-	thinking(philo);
+	while (1)
+	{
+		pthread_mutex_lock(philo->right_fork);
+		take_a_fork(philo);
+		pthread_mutex_lock(philo->left_fork);
+		take_a_fork(philo);
+		eating(philo);
+		pthread_mutex_unlock(philo->right_fork);
+		pthread_mutex_unlock(philo->left_fork);
+		sleeping(philo);
+		thinking(philo);
+	}
 	return (NULL);
 }
 
